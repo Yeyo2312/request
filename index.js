a0f6be113608620e1f92254436f6c0eb69e32847
Diego <cuellardiego2312@gmail.com>
2023-09-19 20:24:31 -0600

    modified:   index.js

--- index.js
+++ index.js
@@ -15,7 +15,20 @@ app.get('/:user/:password',(req,res)=>{
     res.status(404).json({msg: 'Error en el usuario o contraseña'})
 })
 
+//http://localhost:3000/login?user=Diegoypassword=123
+app.get(`/login`,(req,res)=>{//endpoint
+    const {user,password} = req.query
+if (!user|| !password) {
+    req.status(400).json({
+        msg:`you need to provide <user> and <password>parameters`
+    })
+    return
 
+    if(user=== 'Diego' && password ==='123'){
+        res.json({msg: 'Inicio de sesion exitoso'})
+        return
+    }
+})
 
 
 
